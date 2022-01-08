9ab77dafe5f48d16764d8b4550d425f1d0b5cd21
C0DE-AD1CT <80124468+C0DE-AD1CT@users.noreply.github.com>
2022-01-08 22:02:38 +0000

    Update and rename proc.h to code/proc.h

--- code/proc.h
+++ code/proc.h
@@ -1,10 +1,13 @@
-
+// includes
 
 #include<Windows.h>
 #include<TlHelp32.h>
 #include<vector>
 
+//function declarations
 
 DWORD GETPROCID(const wchar_t* procname);
 uintptr_t GetModuleBaseAddress(DWORD procid, const wchar_t* modname);
 uintptr_t FindDMAddy(HANDLE hproc, uintptr_t ptr, std::vector<unsigned int>);
+
+//
